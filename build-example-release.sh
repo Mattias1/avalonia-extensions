606d006749c19afc490a544643c26f57d6e5cd12
Mattias <1116150+Mattias1@users.noreply.github.com>
2024-03-30 18:44:32 +0100

    Update to dotnet 8

--- build-example-release.sh
+++ build-example-release.sh
@@ -6,5 +6,5 @@ rm -rf publish-win-x64
 rm -rf publish-linux-x64
 
 # Build application
-dotnet publish -c Release -o ./publish-win-x64 -f net7.0 -r win-x64 --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
-dotnet publish -c Release -o ./publish-linux-x64 -f net7.0 -r linux-x64 --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish -c Release -o ./publish-win-x64 -f net8.0 -r win-x64 --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish -c Release -o ./publish-linux-x64 -f net8.0 -r linux-x64 --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
